3fe1de38dc57b72288c9357429789b6b9a63c2a5
kamchatka-volcano <gorelyy@vivaldi.net>
2022-10-26 22:42:45 +0300

    -updated the version to 0.12.0; -updated figcone_tree library to 0.12.0; -fixed issues on MSVC compiler; -added Github Actions configuration for CI pipeline;

--- tests/test_datetime.cpp
+++ tests/test_datetime.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <gtest/gtest.h>
 #include <figcone_toml/datetime.h>
 #include <figcone_tree/stringconverter.h>
@@ -15,8 +16,9 @@ TEST(TestDateTime, LocalDate)
     tm.tm_year = 118;
     tm.tm_mon = 11;
     tm.tm_mday = 23;
-    const auto tt = timelocal(&tm);
-    const auto expectedTimePoint = std::chrono::system_clock::from_time_t(tt);
+    const auto t = std::mktime(&tm);
+    const auto tt = std::localtime(&t);
+    const auto expectedTimePoint = std::chrono::system_clock::from_time_t(std::mktime(tt));
     EXPECT_EQ(res->value, expectedTimePoint);
 }
 
@@ -31,9 +33,19 @@ TEST(TestDateTime, LocalDateTime)
     tm.tm_hour = 12;
     tm.tm_min = 30;
     tm.tm_sec = 0;
-    const auto tt = timelocal(&tm);
-    const auto expectedTimePoint = std::chrono::system_clock::from_time_t(tt);
+    const auto t = std::mktime(&tm);
+    const auto tt = std::localtime(&t);
+    const auto expectedTimePoint = std::chrono::system_clock::from_time_t(std::mktime(tt));
     EXPECT_EQ(res->value, expectedTimePoint);
+}
+
+namespace crossplatform
+{
+    std::time_t timegm(std::tm* _Tm)
+    {
+        auto t = std::mktime(_Tm);
+        return t + (std::mktime(std::localtime(&t)) - std::mktime(std::gmtime(&t)));
+    }
 }
 
 TEST(TestDateTime, OffsetDateTime)
@@ -41,14 +53,14 @@ TEST(TestDateTime, OffsetDateTime)
     const auto res = figcone::detail::convertFromString<figcone::toml::DateTimePoint>("2018-12-23T12:30:00+09:30");
     ASSERT_TRUE(res.has_value());
     auto tm = std::tm{};
-    tm.tm_year = 118;
-    tm.tm_mon = 11;
+    tm.tm_year = 2018 - 1900;
+    tm.tm_mon = 12 - 1;
     tm.tm_mday = 23;
     tm.tm_hour = 3;
     tm.tm_min = 0;
     tm.tm_sec = 0;
-    const auto tt = timegm(&tm);
-    const auto expectedTimePoint = std::chrono::system_clock::from_time_t(tt);
+    const auto t = crossplatform::timegm(&tm);
+    const auto expectedTimePoint = std::chrono::system_clock::from_time_t(t);
     EXPECT_EQ(res->value, expectedTimePoint);
 }
 
